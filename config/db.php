ce33ac944207072044c8af96528a07fbec5ce058
asanoturna <gugoan@uol.com.br>
2018-05-20 09:03:33 -0300

    Copy Negocios

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=negocios',
+    'dsn' => 'mysql:host=localhost;dbname=intranet',
     'username' => 'root',
-    'password' => '',
+    'password' => 'ubuntu',
     'charset' => 'utf8',
 ];
